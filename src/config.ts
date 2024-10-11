9e39adf7fa4e745f9ab95bcf8537388ed0654acf
Faustino Luis <billynelson010694@gmail.com>
2024-10-11 12:25:01 +0700

    choreL update default selected tokens

--- src/config.ts
+++ src/config.ts
@@ -68,7 +68,7 @@ export const tokens: Token[] = [
     address: '0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d',
     category: 'popularTokens',
     logo: StarknetLogo,
-    isSelected: true,
+    isSelected: false,
     available: true
   },
   {
@@ -81,7 +81,7 @@ export const tokens: Token[] = [
     address: '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
     category: 'popularTokens',
     logo: ETHLogo,
-    isSelected: true,
+    isSelected: false,
     available: false
   },
   {
@@ -94,7 +94,7 @@ export const tokens: Token[] = [
     address: '0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8',
     category: 'stableTokens',
     logo: USDCLogo,
-    isSelected: true,
+    isSelected: false,
     available: false
   },
   {
@@ -107,7 +107,7 @@ export const tokens: Token[] = [
     address: '0x00da114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3',
     category: 'stableTokens',
     logo: DAILogo,
-    isSelected: true,
+    isSelected: false,
     available: false
   },
   {
@@ -120,7 +120,7 @@ export const tokens: Token[] = [
     address: '0x070a76fd48ca0ef910631754d77dd822147fe98a569b826ec85e3c33fde586ac',
     category: 'stableTokens',
     logo: LUSDLogo,
-    isSelected: true,
+    isSelected: false,
     available: false
   },
   {
